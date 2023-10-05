0a86c9f46f24dfd8fad864f05e3d66c328c55766
Nicholas Bucher <behappy54321@gmail.com>
2023-10-05 15:38:01 -0400

    updated app.js
    Signed-off-by: Nicholas Bucher <behappy54321@gmail.com>

--- app.js
+++ app.js
@@ -7,10 +7,10 @@
 //
 console.log("Starting interval!");
 setInterval(async () => {
-  console.log("Making request...");
+  console.log("Making request at timestamp " + Date.now().valueOf() + "...");
   const res = await fetch(
     "http://example-server-app.apps.svc.cluster.local/example"
   );
   const jsonResponse = await res.json();
   console.log("Recieved data", jsonResponse);
-}, 1000);
+}, 200);
